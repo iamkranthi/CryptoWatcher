c64c9cca59608b3e6f7ada0addfcba39af0f6a26
iamkranthi <kranthigowthamkumar@gmail.com>
2023-02-18 15:05:23 +0530

    feat: Tab bar view added

---
+++ lib/pages/favrouites.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Favrouites extends StatelessWidget {
+  const Favrouites({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Expanded(
+      child: Text("data"),
+    );
+  }
+}
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,8 +1,7 @@
 import 'package:crypto_currency_price_tracker/helpers/helpers.dart';
 import 'package:crypto_currency_price_tracker/helpers/responsive.dart';
-import 'package:crypto_currency_price_tracker/models/crypto_currency.dart';
-import 'package:crypto_currency_price_tracker/pages/detail_page.dart';
-import 'package:crypto_currency_price_tracker/providers/market_provider.dart';
+import 'package:crypto_currency_price_tracker/pages/favrouites.dart';
+import 'package:crypto_currency_price_tracker/pages/markets.dart';
 import 'package:crypto_currency_price_tracker/providers/theme_provider.dart';
 import 'package:crypto_currency_price_tracker/widgets/text.dart';
 import 'package:flutter/material.dart';
@@ -15,7 +14,15 @@ class Homepage extends StatefulWidget {
   State<Homepage> createState() => _HomepageState();
 }
 
-class _HomepageState extends State<Homepage> {
+class _HomepageState extends State<Homepage> with TickerProviderStateMixin {
+  late TabController viewController;
+
+  @override
+  void initState() {
+    super.initState();
+    viewController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     Themeprovider themeProvider =
@@ -53,7 +60,7 @@ class _HomepageState extends State<Homepage> {
                       )
                     ],
                   ),
-                  verticalSpacer(8),
+                 
                   Headings(
                     text: "Welcome Back",
                     fontsize: 20,
@@ -61,115 +68,29 @@ class _HomepageState extends State<Homepage> {
                     textScaleFactor: responsive.sp(3),
                   ),
                   verticalSpacer(10),
-                  Flexible(
-                    child: Consumer<MarketProvider>(
-                      builder: (context, marketProvider, child) {
-                        if (marketProvider.isLoading == true) {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        } else {
-                          if (marketProvider.markets.isNotEmpty) {
-                            return RefreshIndicator(
-                              onRefresh: () async {
-                                await marketProvider.fetchData();
-                              },
-                              child: ListView.builder(
-                                physics: const BouncingScrollPhysics(
-                                    parent: AlwaysScrollableScrollPhysics()),
-                                itemCount: marketProvider.markets.length,
-                                itemBuilder: ((context, index) {
-                                  CryptoCurrency currentCrypto =
-                                      marketProvider.markets[index];
-                                  return Card(
-                                    elevation: 4,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(16.0),
-                                    ),
-                                    margin: const EdgeInsets.symmetric(
-                                        vertical: PADDINGS.padding2,
-                                        horizontal: PADDINGS.padding3),
-                                    child: ListTile(
-                                        onTap: () {
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) => Details(
-                                                  id: currentCrypto.id!),
-                                            ),
-                                          );
-                                        },
-                                        contentPadding:
-                                            const EdgeInsets.symmetric(
-                                                horizontal: 8),
-                                        leading: CircleAvatar(
-                                          backgroundColor: Colors.black,
-                                          backgroundImage: NetworkImage(
-                                              currentCrypto.image!),
-                                        ),
-                                        title: Text(currentCrypto.name!),
-                                        subtitle: Text(
-                                          currentCrypto.symbol!.toUpperCase(),
-                                          style: const TextStyle(
-                                              color: Colors.green),
-                                        ),
-                                        trailing: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceEvenly,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.end,
-                                          children: [
-                                            Headings(
-                                              text:
-                                                  "₹ ${currentCrypto.currentPrice!.toStringAsFixed(2)}",
-                                              fontsize: 17,
-                                              fontWeight: FontWeight.w500,
-                                              color: Colors.blueGrey,
-                                            ),
-                                            Builder(
-                                              builder: (context) {
-                                                double priceChange =
-                                                    currentCrypto
-                                                        .priceChange24!;
-                                                double priceChangePercentage =
-                                                    currentCrypto
-                                                        .priceChangePercentage24!;
-                                                if (priceChange < 0) {
-                                                  return Headings(
-                                                    text:
-                                                        "₹ ${priceChangePercentage.toStringAsFixed(2)} % (${priceChange.toStringAsFixed(2)})",
-                                                    fontsize: 12,
-                                                    fontWeight: FontWeight.w400,
-                                                    color: Colors.red,
-                                                  );
-                                                } else if (priceChange > 0) {
-                                                  return Headings(
-                                                    text:
-                                                        "₹ ${priceChangePercentage.toStringAsFixed(2)} % (${priceChange.toStringAsFixed(2)})",
-                                                    fontsize: 12,
-                                                    fontWeight: FontWeight.w400,
-                                                    color: Colors.blue,
-                                                  );
-                                                } else {
-                                                  return Container();
-                                                }
-                                              },
-                                            ),
-                                          ],
-                                        )
-                                        //Text(currentCrypto.currentPrice!.toStringAsFixed(2),s),
-                                        ),
-                                  );
-                                }),
-                              ),
-                            );
-                          } else {
-                            return const Text("Data not found");
-                          }
-                        }
-                      },
-                    ),
+                  TabBar(
+                    controller: viewController,
+                    tabs: const [
+                      Tab(
+                        child: Text("Markets"),
+                      ),
+                      Tab(
+                        child: Text("Favrouites"),
+                      ),
+                    ],
                   ),
+                  Expanded(
+                    child: TabBarView(
+                      physics: const BouncingScrollPhysics(
+                        parent: AlwaysScrollableScrollPhysics()
+                      ),
+                      controller: viewController,
+                      children: const [
+                        Markets(),
+                        Favrouites(),
+                      ],
+                    ),
+                  )
                 ],
               ),
             );
